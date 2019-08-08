cbeb9b55a917d7119c54b451bfcbf54eddd35983
Alexander Webber <ajtwebber@gmail.com>
2019-08-08 12:38:23 +0100

    Initial thoughts  (#1)
    * Initial changes
    
    * Remove CR from input lines before extracting their content
    
    Tested under both Windows and Linux dev environments.

--- OCRManager/Impl/OcrService.cs
+++ OCRManager/Impl/OcrService.cs
@@ -13,30 +13,20 @@ namespace OCRManager.Impl
             _ocrParser = new OcrParser();
         }
 
-        public bool IsValidOcr(string parsedOcr)
-        {
-            ICollection<int> ocr = parsedOcr.Select(ch => int.Parse(ch.ToString())).ToList();
-
-            int sum = 0;
-            for (int i = 1; i < 10; ++i)
-            {
-                sum += i * ocr.ElementAt(ocr.Count - i);
-            }
-
-            return sum % 11 == 0;
-        }
+        public bool IsValidAccountNumber(string parsedOcr) => 
+            IsValidAccountNumber(parsedOcr.Select(x => int.Parse(x.ToString())));
 
         public string ValidateOcr(string input)
         {
-            string ocr = _ocrParser.ParseInput(input);
+            var ocr = _ocrParser.ParseInput(input);
 
-            StringBuilder result = new StringBuilder(ocr);
+            var result = new StringBuilder(ocr);
 
             if (ocr.Contains(_ocrParser.UnknownDigit))
             {
                 result.Append(" ILL");
             }
-            else if (!IsValidOcr(ocr))
+            else if (!IsValidAccountNumber(ocr))
             {
                 result.Append(" ERR");
             }
@@ -46,98 +36,77 @@ namespace OCRManager.Impl
 
         public string ValidateAndTryToFixOcr(string input)
         {
-            string ocr = _ocrParser.ParseInput(input);
+            var ocr = _ocrParser.ParseInput(input);
 
-            if (!ocr.Contains(_ocrParser.UnknownDigit) && IsValidOcr(ocr))
+            if (!ocr.Contains(_ocrParser.UnknownDigit) && IsValidAccountNumber(ocr))
             {
                 return ocr;
             }
-            else if (!ocr.Contains(_ocrParser.UnknownDigit))
-            {
-                return AmmendWellFormedOcr(ocr);
-            }
-            else
-            {
-                return AmmendIllFormedOcr(input, ocr);
-            }
+
+            return !ocr.Contains(_ocrParser.UnknownDigit) 
+                ? AmendWellFormedOcr(ocr) 
+                : AmendIllFormedOcr(input, ocr);
         }
 
-        private string AmmendWellFormedOcr(string wrongOcr)
-        {
-            List<string> candidateOcrs = new List<string>();
-            List<int> parsedOcr = wrongOcr.Select(ch => int.Parse(ch.ToString())).ToList();
+        private static bool IsValidAccountNumber(IEnumerable<int> accountNumber) =>
+            accountNumber
+                .Reverse()
+                .Select((x, i) => (i + 1) * x)
+                .Sum() % 11 == 0;
 
-            for (int digitIndex = 0; digitIndex < parsedOcr.Count; ++digitIndex)
+        private static string AmendWellFormedOcr(string wrongOcr)
+        {
+            var candidateAccountNumbers = new List<string>();
+            var accountNumber = wrongOcr.Select(ch => int.Parse(ch.ToString())).ToList();
+
+            for (int digitIndex = 0; digitIndex < accountNumber.Count; ++digitIndex)
             {
-                int digitToModify = parsedOcr[digitIndex];
+                int digitToModify = accountNumber[digitIndex];
 
                 var alternativeDigits = DigitHelper.GetSimilarDigits(digitToModify);
                 foreach (int alternativeDigit in alternativeDigits)
                 {
-                    parsedOcr[digitIndex] = alternativeDigit;
+                    accountNumber[digitIndex] = alternativeDigit;
 
-                    int sum = 0;
-                    for (int i = 1; i < 10; ++i)
+                    if (IsValidAccountNumber(accountNumber))
                     {
-                        sum += i * parsedOcr[parsedOcr.Count - i];
-                    }
-
-                    if (sum % 11 == 0)
-                    {
-                        candidateOcrs.Add(string.Join(string.Empty, parsedOcr));
+                        candidateAccountNumbers.Add(string.Join(string.Empty, accountNumber));
                     }
 
 #pragma warning disable S4143 // Collection elements should not be replaced unconditionally - Intended behavior
-                    parsedOcr[digitIndex] = digitToModify;
+                    accountNumber[digitIndex] = digitToModify;
 #pragma warning restore S4143 // Collection elements should not be replaced unconditionally
                 }
             }
 
-            switch (candidateOcrs.Count)
+            switch (candidateAccountNumbers.Count)
             {
                 case 0:
                     throw new OcrParsingException("Error in AmmendWrongChecksum, something's wrong");
                 case 1:
-                    return string.Join(string.Empty, candidateOcrs.First());
+                    return string.Join(string.Empty, candidateAccountNumbers.First());
                 default:
-                    StringBuilder result = new StringBuilder(wrongOcr);
-
-                    result.Append(" AMB [");
-                    foreach (string candidate in candidateOcrs.OrderBy(str => str))
-                    {
-                        result.Append($"'{candidate}', ");
-                    }
-                    result.Append("]");
-                    result.Replace(", ]", "]");
-
-                    return result.ToString();
+                    return $"{wrongOcr} AMB ['{string.Join("', '",  candidateAccountNumbers.OrderBy(str => str))}']";
             }
         }
 
-        private string AmmendIllFormedOcr(string input, string wrongOcr)
+        private string AmendIllFormedOcr(string input, string wrongOcr)
         {
             int illFormedDigitIndex = wrongOcr.IndexOf(_ocrParser.UnknownDigit);
-            const string sampleDigit = "9";
-            wrongOcr = wrongOcr.Replace(_ocrParser.UnknownDigit, sampleDigit);
-            List<string> candidateOcrs = new List<string>();
-            List<int> parsedOcr = wrongOcr.Select(ch => int.Parse(ch.ToString())).ToList();
-
-            string illFormedDigit = _ocrParser.ExtractIllFormedDigit(input);
-
+            wrongOcr = wrongOcr.Replace(_ocrParser.UnknownDigit, "0");
+            var accountNumber = wrongOcr.Select(ch => int.Parse(ch.ToString())).ToList();
+            var illFormedDigit = _ocrParser.ExtractIllFormedDigit(input);
             var alternativeDigits = DigitHelper.GetSimilarDigits(illFormedDigit);
+
+            var candidateOcrs = new List<string>();
+
             foreach (int alternativeDigit in alternativeDigits)
             {
-                parsedOcr[illFormedDigitIndex] = alternativeDigit;
+                accountNumber[illFormedDigitIndex] = alternativeDigit;
 
-                int sum = 0;
-                for (int i = 1; i < 10; ++i)
+                if (IsValidAccountNumber(accountNumber))
                 {
-                    sum += i * parsedOcr[parsedOcr.Count - i];
-                }
-
-                if (sum % 11 == 0)
-                {
-                    candidateOcrs.Add(string.Join(string.Empty, parsedOcr));
+                    candidateOcrs.Add(string.Join(string.Empty, accountNumber));
                 }
             }
 
